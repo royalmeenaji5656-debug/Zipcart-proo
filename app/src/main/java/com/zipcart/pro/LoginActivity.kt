c50b74c35c66e7a22539ab7250004490bda0c49a
royalmeenaji5656-debug <royalmeenaji5656@gmail.com>
2026-05-25 11:24:36 +0530

    Add LoginActivity for user authentication

---
+++ app/src/main/java/com/zipcart/pro/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.zipcart.pro
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
